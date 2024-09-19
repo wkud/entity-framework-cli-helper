9687f17dd162fadd5c1a869de43dd222302d3b93
Wojciech <vopox07@gmail.com>
2024-09-19 16:24:30 +0200

    Update shorter and cleaned command

--- ef.your-project.cmd
+++ ef.your-project.cmd
@@ -4,37 +4,6 @@ REM Settings
 set project="Infrastructure"
 set statup-project="Api"
 
-REM Check if there are at least two arguments
-if "%1"=="" (
-    echo Please provide a command and subcommand.
-    exit /b 1
-)
-if "%2"=="" (
-    echo Please provide a subcommand.
-    exit /b 1
-)
+set dotnetCommand=dotnet ef %* --project %project% --startup-project %startup-project%
+%dotnetCommand%
 
-REM Assign the first and second arguments as the command and subcommand
-set command=%1
-set subcommand=%2
-shift
-shift
-
-REM Construct the dotnet ef command
-set dotnetCommand=dotnet ef %command% %subcommand% --project %project% --startup-project %startup-project%
-REM Initialize the remaining arguments variable
-set remainingArgs=
-
-REM Loop through the remaining arguments and append them
-:loop
-if "%1"=="" goto execute
-set remainingArgs=%remainingArgs% %1
-shift
-goto loop
-
-:execute
-REM Change directory to src
-cd src
-
-REM Execute the command with the remaining arguments
-%dotnetCommand% %remainingArgs%
